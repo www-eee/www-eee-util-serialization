08fb346b62566197c9ccedd17dc365dcfe83fa2d
Chris Hubick <chris@hubick.com>
2018-09-04 16:02:10 -0600

    Update to Java 11.

--- main/src/main/java/module-info.java
+++ main/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module net.www_eee.util.serialization {
   requires transitive org.eclipse.jdt.annotation;
   requires transitive java.xml;
-  requires java.xml.ws;
+  requires java.xml.soap;
 
   exports net.www_eee.util.serialization.introspection;
   exports net.www_eee.util.serialization.xml;
--- ws-rs/src/main/java/module-info.java
+++ ws-rs/src/main/java/module-info.java
@@ -1,9 +1,8 @@
 module net.www_eee.util.serialization.ws {
   requires transitive org.eclipse.jdt.annotation;
-  requires transitive java.xml;
-  requires transitive java.xml.ws;
   requires transitive java.ws.rs;
   requires transitive net.www_eee.util.serialization;
+  requires java.xml.soap;
   requires java.naming;
 
   exports net.www_eee.util.serialization.ws.rs.provider.xml;
