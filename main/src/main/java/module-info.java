8db8c89a6fea1021659068f76467e608871c2027
Chris Hubick <chris@hubick.com>
2018-09-13 15:08:20 -0600

    Backport 1.2 to 1.1 for Java 8 compatibility.

--- main/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module net.www_eee.util.serialization {
-  requires transitive org.eclipse.jdt.annotation;
-  requires transitive java.xml;
-  requires java.xml.soap;
-
-  exports net.www_eee.util.serialization.introspection;
-  exports net.www_eee.util.serialization.xml;
-}
