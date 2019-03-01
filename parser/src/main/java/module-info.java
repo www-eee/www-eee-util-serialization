a50b484ae5b486335f44485da5e091dc3a27921d
Chris Hubick <chris@hubick.com>
2019-02-28 18:56:33 -0700

    Switch from jaxws-ri to jaxws-rt and exclude unneeded transitive deps.

--- parser/src/main/java/module-info.java
+++ parser/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module net.www_eee.util.serialization.parser {
   requires transitive org.eclipse.jdt.annotation;
   requires transitive java.xml;
+  requires transitive java.desktop;
   requires java.xml.soap;
   requires java.xml.ws;
   requires org.jooq;
